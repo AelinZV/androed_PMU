aa78e46b2bf3f72b40af4711b4866dead51ed8ae
Михайлов Руслан Александрович <AelinZV@github.com>
2026-09-13 15:04:35 +0700

    структура для рег. игрока

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
