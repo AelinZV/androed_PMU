012ad5c33a237d8cceaab3a22468260d66861a04
Михайлов Руслан Александрович <AelinZV@github.com>
2026-09-19 23:06:07 +0700

    теперь структура игрока хранится отдельно в Player.kt

---
+++ app/src/main/java/com/example/myapplication/Player.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+data class Player(
+    val fullName: String,
+    val gender: String,
+    val course: String,
+    val difficulty: String,
+    val birthDate: String,
+    val zodiac: String
+)
