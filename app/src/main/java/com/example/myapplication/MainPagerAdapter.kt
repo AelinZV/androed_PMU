992423eb63c122b25e6d7df68ccf465d1e8d5f45
Михайлов Руслан Александрович <AelinZV@github.com>
2026-09-19 23:04:47 +0700

    создан MainPagerAdapter.kt - выбор фрагмента при выборе

---
+++ app/src/main/java/com/example/myapplication/MainPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainPagerAdapter(
+    activity: FragmentActivity
+) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun createFragment(position: Int): Fragment {
+
+        return when (position) {
+
+            0 -> RegistrationFragment()
+
+            1 -> RulesFragment()
+
+            2 -> AuthorsFragment()
+
+            3 -> SettingsFragment()
+
+            else -> RegistrationFragment()
+        }
+    }
+}
