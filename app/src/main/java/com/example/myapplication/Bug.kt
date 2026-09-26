5ec498419dbb5cdb2368bc7dd74294ca71f360fc
Михайлов Руслан Александрович <AelinZV@github.com>
2026-09-26 14:13:09 +0700

    хранение информации об отдельном насекомом.

---
+++ app/src/main/java/com/example/myapplication/Bug.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.widget.ImageView
+
+data class Bug(
+    val imageView: ImageView,
+
+    var x: Float,
+    var y: Float,
+
+    var dx: Float,
+    var dy: Float
+)
